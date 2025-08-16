222efe46406dcbf6f9129628b1129e8c8bd031a1
Huseyn <hsbziyev@gmail.com>
2025-08-16 12:12:00 +0400

    SQL Query added successfully

---
+++ SQLQuery2.sql
@@ -0,0 +1,12 @@
+﻿
+
+--CREATE TABLE [USER]
+--(
+--[Id] int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+--[UserName] nvarchar(50) NOT NULL UNIQUE,
+--[Password] nvarchar(50) NOT NULL,
+--[FirstName] nvarchar(50) NOT NULL,
+--[LastName] nvarchar(50) NOT NULL,
+--[Age] tinyint NOT NULL,
+--[Gender] bit NOT NULL,
+--)
